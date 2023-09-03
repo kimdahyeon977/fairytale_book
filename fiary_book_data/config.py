ca2ad6f0568b1ece2ba3f1db864c82eac5a26356
Park Jeong Hwan <jpark0902@kookmin.ac.kr>
2023-09-03 15:55:07 +0900

    [Add] 도커 파일, API 키 설정 추가, 패키지 의존성 명시

--- fiary_book_data/config.py
+++ fiary_book_data/config.py
@@ -1,1 +1,13 @@
+class Config(object):
+    DEBUG = True
+    TESTING = False
 
+class DevelopmentConfig(Config):
+    SECRET_KEY = ""
+    OPENAI_KEY = ""
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': DevelopmentConfig,
+    'production': DevelopmentConfig
+}
