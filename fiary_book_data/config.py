d8a7a7b370640bd707e59a2b5a3aab1c54c8f9c6
inth9198 <82989054+inth9198@users.noreply.github.com>
2023-09-03 13:00:46 +0900

    Update config.py
    Signed-off-by: inth9198 <82989054+inth9198@users.noreply.github.com>

--- fiary_book_data/config.py
+++ fiary_book_data/config.py
@@ -1,13 +1,1 @@
-class Config(object):
-    DEBUG = True
-    TESTING = False
 
-class DevelopmentConfig(Config):
-    SECRET_KEY = 'jhfduhfd8754827ywehujkfh977428T%^#@#TYHGF55t7yhlgjhhsddfda'
-    OPENAI_KEY = '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'
-
-config = {
-    'development': DevelopmentConfig,
-    'testing': DevelopmentConfig,
-    'production': DevelopmentConfig
-}
